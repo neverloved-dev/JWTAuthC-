4e48e771825095bf3168a18c5aa9b9743a7e0569
FinGY <djordje.andjic99@outlook.com>
2022-09-17 19:41:18 +0200

    transfered to code

---
+++ Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BAuth.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAll()
+        {
+            var users = new List<User>()
+            {
+                new User
+                {
+                    Name = "John",
+                    Password = "pass1"
+                 },
+                new User
+                    {
+                        Name = "Dave",
+                         Password = "pass11"
+                    }
+
+            };
+            return Ok(users);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BAuth
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password {get;set;}
+    }
+}
