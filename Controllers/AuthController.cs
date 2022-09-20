8c856f40a79b0946f58156f716a89d087e6b6195
GeorgeAnzhich <djordjeandjic99@gmail.com>
2022-09-20 13:25:05 +0200

    Finished the auth logic

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using System.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace BAuth.Controllers
 {
@@ -9,10 +13,15 @@ namespace BAuth.Controllers
     public class AuthController : Controller
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(UserDto request)
         {
-            CreatePasswordHash(request.Password,out byte[] passwordHash,out byte[] passwordSalt);
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
             user.Name = request.Name;
             user.PasswordSalt = passwordSalt;
             user.PasswordHash = passwordHash;
@@ -20,38 +29,55 @@ namespace BAuth.Controllers
 
         }
 
-         [HttpPost("login")]
+        [HttpPost("login")]
         public async Task<ActionResult<User>> LogIn(UserDto request)
         {
-            if(user.Name != request.Name)
+            if (user.Name != request.Name)
             {
                 return BadRequest("User not found!");
             }
-            if(!VerifyPassword(request.Password, user.PasswordHash,user.PasswordSalt))
+            if (!VerifyPassword(request.Password, user.PasswordHash, user.PasswordSalt))
             {
                 return BadRequest("Wrong password!");
             }
-            var token = CreateToken()
+            var token = CreateToken(user);
             return Ok(token);
         }
 
-          private void CreatePasswordHash(string password, out byte[]passwordHash, out byte[]PasswordSalt)
-         {
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] PasswordSalt)
+        {
             using (var hmac = new HMACSHA256())
             {
                 PasswordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
-         }
-         private bool VerifyPassword(string password, out byte[]passwordHash, out byte[]PasswordSalt)
-         {
+        }
+        private bool VerifyPassword(string password, byte[] passwordHash, byte[] PasswordSalt)
+        {
             using (var hmac = new HMACSHA256(PasswordSalt))
             {
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                return computedHash.SequenceEqual(passwordHash); // Not working
+                return computedHash.SequenceEqual(passwordHash);
             }
-         }
+        }
 
-  
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Name),
+            };
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(10),
+                signingCredentials: cred);
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
     }
 }
+
